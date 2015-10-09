411357ed2bfe6e1a2555f2b7a9603c8674bc5072
micuat <micuat@gmail.com>
2015-10-09 17:33:16 -0400

    first commit

---
+++ Assets/Rug.Osc/Scripts/ReceiveOscBehaviourBase.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using Rug.Osc;
+
+public abstract class ReceiveOscBehaviourBase : MonoBehaviour {
+
+	private OscReceiveController m_ReceiveController; 
+
+	public GameObject ReceiveControllerObject; 
+
+	public string OscAddress = "/test";
+
+	public void Awake () {
+		
+		m_ReceiveController = null; 
+		
+		if (ReceiveControllerObject == null) {
+			Debug.LogError("You must supply a ReceiverControllerObject"); 
+			return; 
+		}
+
+		OscReceiveController controller = ReceiveControllerObject.GetComponent<OscReceiveController> (); 
+		
+		if (controller == null) { 
+			Debug.LogError(string.Format("The GameObject with the name '{0}' does not contain a OscReceiveController component", ReceiveControllerObject.name)); 
+			return; 
+		}
+		
+		m_ReceiveController = controller; 
+	}
+
+	// Use this for initialization
+	public virtual void Start () {
+
+		if (m_ReceiveController != null) {
+
+			m_ReceiveController.Manager.Attach (OscAddress, ReceiveMessage); 
+		}
+	}
+
+	// Update is called once per frame
+	public virtual void Update () {
+	
+	}
+
+	public virtual void OnDestroy () {
+
+		// detach from the OscAddressManager
+		if (m_ReceiveController != null) {
+			m_ReceiveController.Manager.Detach (OscAddress, ReceiveMessage);
+		}
+	}
+
+	protected abstract void ReceiveMessage (OscMessage message);
+}
---
+++ Assets/Scripts/FloorReceiver.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rug.Osc;
+
+public class FloorReceiver : ReceiveOscBehaviourBase {
+
+	public GameObject chunk;
+	bool[] spawened = new bool[4];
+
+	protected override void ReceiveMessage (OscMessage message) {
+
+		if (message.Count != 16) 
+		{
+			Debug.LogError(string.Format("Unexpected argument count {0}", message.Count));  
+			
+			return; 
+		}
+
+		int[] fsrTile = new int[4];
+		int[,] fsrRaw = new int[4, 4];
+		for(int i = 0; i < 4; i++)
+		{
+			fsrTile[i] = 0;
+			for(int j = 0; j < 4; j++)
+			{
+				fsrTile[i] += (int)message[i * 4 + j];
+				fsrRaw[i, j] = (int)message[i * 4 + j];
+			}
+		}
+
+		Debug.Log(fsrTile[0] + " " + fsrTile[1] + " " + fsrTile[2] + " " + fsrTile[3]); 
+
+		for (int i = 0; i < 4; i++) {
+			Vector3 cmass = new Vector3();
+			Vector3 tileCenter = new Vector3();
+			switch(i) {
+			case 0:
+				tileCenter += new Vector3( 0.5f, 0, -0.5f);
+				break;
+			case 1:
+				tileCenter += new Vector3( 0.5f, 0,  0.5f);
+				break;
+			case 2:
+				tileCenter += new Vector3(-0.5f, 0, -0.5f);
+				break;
+			case 3:
+				tileCenter += new Vector3(-0.5f, 0, 0.5f);
+				break;
+			}
+			cmass += (new Vector3(-1, 0, -1) * 0.46f + tileCenter) * fsrRaw[i, 0];
+			cmass += (new Vector3(-1, 0,  1) * 0.46f + tileCenter) * fsrRaw[i, 1];
+			cmass += (new Vector3( 1, 0,  1) * 0.46f + tileCenter) * fsrRaw[i, 2];
+			cmass += (new Vector3( 1, 0, -1) * 0.46f + tileCenter) * fsrRaw[i, 3];
+			if (fsrTile [i] > 20000 && spawened[i] == false) {
+				var c = Instantiate(chunk, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+				c.GetComponent<Rigidbody>().AddForce(new Vector3(0, 200, 0) + cmass * 0.005f);
+				spawened[i] = true;
+			} else if (fsrTile [i] <= 20000) {
+				spawened[i] = false;
+			}
+		}
+	}
+}
