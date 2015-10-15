045ae692f5d865c8b56e7963a5bc5f9216a8eecc
Naiko90 <nicola.gallo@mail.mcgill.ca>
2015-10-15 19:12:02 -0400

    UI design

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test : MonoBehaviour {
+public class CollisionManager : MonoBehaviour {
 
     bool isHit = false;
     public int Time = 5;
---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GUIManager : MonoBehaviour
+{
+    static public GUIManager instance;
+
+    [Header("Ingame Menu")]
+    public GameObject inGameTopPanel;
+    public Text inGameTimeText;
+
+    float time = 30;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Hide everything except the main menu
+        //inGameTopPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            SetInGameTime(time);
+        }
+    }
+
+    public void SetInGameTime(float score)
+    {
+        inGameTimeText.text = score.ToString("0.00");
+    }
+}
